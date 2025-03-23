2fe78fa63f96a09770d2e5470bef7845a4af2185
Caixian-Wang <20108795@mail.wit.ie>
2025-03-23 02:34:47 +0000

    Create an API Gateway and configure CORS

--- lib/assignment1-stack.ts
+++ lib/assignment1-stack.ts
@@ -61,6 +61,19 @@ export class Assignment1Stack extends cdk.Stack {
     });
     this.table.grantReadWriteData(translateFunction);
 
-
+    const api = new apigateway.RestApi(this, "Assignment1Api", {
+      description: "Serverless REST API for Assignment1: supports CRUD operations and text translation caching",
+      deployOptions: {
+        stageName: "dev",
+        loggingLevel: apigateway.MethodLoggingLevel.INFO,
+        dataTraceEnabled: true,
+      },
+      defaultCorsPreflightOptions: {
+        allowHeaders: ["Content-Type", "X-Amz-Date", "Authorization", "x-api-key"],
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowCredentials: true,
+        allowOrigins: ["*"],
+      },
+    });
   }
 }
